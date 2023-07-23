083f363a17469584f98e4299a204a824dfaa92a9
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-07-23 12:43:10 +0300

    Add getById in userService

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,3 +1,12 @@
+const baseUrl = 'http://localhost:3005/api/users';
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+
+    return result.users;
+};
+
 export const getById = async (userId) => {
     const response = await fetch(`${baseUrl}/${userId}`);
     const result = await response.json();
