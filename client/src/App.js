6399f58750e784b238f2a20acf303d8d682bf752
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-07-23 12:44:16 +0300

    Add UserDetails components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService';
+
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Search } from './components/Search';
+import { UserList } from './components/UserList';
+
 import './App.css';
 
 function App() {
+    const [users, setUsers] = useState([]); 
+
+    useEffect(() => {
+        userService.getAll()
+            .then(users => {
+                setUsers(users);
+            })
+            .catch(err => {
+                console.log('Error: ' + err);
+            });
+    });
+
     return (
         <>
             <Header />
 
             <main className="main">
-                <Search />
+                <section className="card users-container">
+                    <Search />
+                    <UserList users={users} />
+
+                    <button class="btn-add btn">Add new user</button>
+                    
+                </section>
             </main>
 
             <Footer />
