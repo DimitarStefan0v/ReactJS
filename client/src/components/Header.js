c9c243135b9b441120fadea12b4c7ddbad1b366c
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-07-23 10:04:56 +0300

    Add Header and Footer components

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <header class="header">
+            <div class="logo">
+                <span class="course">react course</span>
+                <span class="description">user list demo</span>
+            </div>
+        </header>
+    );
+};
