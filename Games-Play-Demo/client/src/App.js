b547b2c9eada75c58d3c14fa6395ce63be69a41c
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-08-12 16:06:14 +0300

    Add Catalog component

--- Games-Play-Demo/client/src/App.js
+++ Games-Play-Demo/client/src/App.js
@@ -2,6 +2,8 @@ import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
+import { CreateGame } from "./components/CreateGame/CreateGame";
+import { Catalog } from "./components/Catalog/Catalog";
 
 function App() {
     return (
