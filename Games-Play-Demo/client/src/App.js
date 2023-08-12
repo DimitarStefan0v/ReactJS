7435c0c64dd2bdfd838baa276ef975c02103dd15
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-08-12 16:01:12 +0300

    Add Register component

--- Games-Play-Demo/client/src/App.js
+++ Games-Play-Demo/client/src/App.js
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
 
 function App() {
     return (
