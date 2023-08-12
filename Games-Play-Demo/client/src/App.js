285097d603f9a2c60025ef2a00228d246dff1606
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-08-12 17:56:30 +0300

    Add getAll service for games

--- Games-Play-Demo/client/src/App.js
+++ Games-Play-Demo/client/src/App.js
@@ -1,13 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { Header } from "./components/Header/Header";
-import { Home } from "./components/Home/Home";
-import { Login } from "./components/Login/Login";
-import { Register } from "./components/Register/Register";
-import { CreateGame } from "./components/CreateGame/CreateGame";
-import { Catalog } from "./components/Catalog/Catalog";
+import * as gameService from './services/gameService';
+
+import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { CreateGame } from './components/CreateGame/CreateGame';
+import { Catalog } from './components/Catalog/Catalog';
 
 function App() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result)
+            })
+    }, []);
+
     return (
         <div id="box">
             <Header />
